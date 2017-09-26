e8766689dc51122dbfe935e241c5355e00f9f94d
Emilie Rabeau <rabe0019@algonquinlive.com>
2017-09-26 15:02:44 -0400

    final exercises

--- Converter.py
+++ Converter.py
@@ -15,14 +15,22 @@ def acres_to_edge_of_square(acres):
     m = (meterSq) ** 0.5
     return m
 
-def get_bear_count(beardesnity):
-    return
+def get_bear_count(bear_d,a_sqmetres):
+    get_bear_count = (a_sqmetres * 0.000001) * (bear_d)
+    return get_bear_count
 
-def dms_to_dd(dms):
-    return
+def dms_to_dd(degree,min,sec):
+    dms_to_dd = degree + (min/60.000) + (sec/3600.000)
+    return dms_to_dd
 
 def dd_to_dms(dd):
-    return
+    degrees = int(dd)
+    dd_minutes = (dd - degrees) * 60
+    minutes = int(dd_minutes)
+    seconds  = round((dd_minutes - minutes) * 60)
+    dd_to_dms = str(dd) + " = " + str(degrees) + ":" + str(minutes) + ":" + str(seconds)
+    return dd_to_dms
 
-def get_fuel_cost(m):
-    return
+def get_fuel_cost(km,mpg,dl):
+    get_fuel_cost = (((km / 1.6) / mpg) * 3.78)* dl
+    return get_fuel_cost
--- ConverterTest.py
+++ ConverterTest.py
@@ -11,4 +11,10 @@ print "10000 square metres = " + str(Converter.sqmetres_to_acres(10000)) + " acr
 
 print "Edge length of a 2 acres square is " + str(Converter.acres_to_edge_of_square(2)) + " metres."
 
-print "When bear density is 4 bears per square km and area is"
+print "When bear density is 4 bears per square km and area is 10 000 000 square metres, the probable number of bears is " + str(Converter.get_bear_count(4,10000000))
+
+print "95:25:5 = " + str(Converter.dms_to_dd(95,25,5))
+
+print str(Converter.dd_to_dms(95.41805))
+
+print "100 km at 35 mi/gal and $ 1.30 per L will cost $" + str(Converter.get_fuel_cost(100,35,1.30))
